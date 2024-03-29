9bde1107d8d41d35e0aecddcd9eff790241f298d
Kanish Chheda <kanishchheda@gmail.com>
2024-03-30 04:36:10 +0530

    Sidebar and navbar completed

---
+++ components/bars/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import MobileNav from '@/components/bars/MobileNav'
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 flex w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Focus Meet
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        {/*  Clerk user management */}
+        <MobileNav />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
